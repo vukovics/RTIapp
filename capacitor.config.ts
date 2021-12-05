99d37bcfd9c29e9a4a10b31c4a814a92864f5213
Stefan Vukovic <stefanvkvc@gmail.com>
2021-12-05 11:28:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Walk',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
